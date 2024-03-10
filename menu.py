6414fa9fc61c8c57869b0496e13c97c0cd3ba480
andres22 <afpinchao8@misena.edu.co>
2024-03-10 18:46:39 -0500

    finishing our menu interface

--- menu.py
+++ menu.py
@@ -4,13 +4,38 @@ import os
 def iniciar():
   while True:
     #borramos lo que estemos ejecutando continuamente en terminal
-    os.system('cls')
+    os.system('clear')
     print("================")
     print(" Bienvenido al Gestor")
     print("================")
     print("[1] Listar los clientes ")
-    print("[2] Listar los clientes ")
-    print("[3] Listar los clientes ")
-    print("[4] Listar los clientes ")
-    print("[5] Listar los clientes ")
-    print("[6] Listar los clientes ")
+    print("[2] Buscar un cliente ")
+    print("[3] Anadir un cliente")
+    print("[4] Modificar un cliente")
+    print("[5] Borrar un cliente")
+    print("[6] Cerrar el Gestor")
+    print("================")
+    
+    opcion = input(">")
+    os.system('clear')
+    
+    if opcion == '1':
+      print("Listando los clientes...\n")
+      #todo
+    elif opcion == '2':
+      print("Buscando un cliente...\n")
+      #todo
+    elif opcion == '3':
+      print("Anadiendo un cliente...\n")
+      #todo
+    elif opcion == '4':
+      print("Modificando un cliente...\n")
+      #todo
+    elif opcion == '5':
+      print("Borrando un cliente...\n")
+      #todo
+    elif opcion == '6':
+      print("Saliendo")
+      break
+    
+  input("\n Presiona ENTER para continuar..")
--- run.py
+++ run.py
@@ -1,1 +1,9 @@
 #en este script sera el que ejecutara toda la aplicacion
+
+#importamos el menu
+import menu 
+
+#comprobamos que se esta ejecutando desde el principal
+if __name__ == "__main__":
+  #del modulo menu ejecutamos la funcion iniciar 
+  menu.iniciar()
