ffd333c73dbc3154404468d10007b2afc674f5a9
andres22 <afpinchao8@misena.edu.co>
2024-05-07 22:17:48 -0500

    configure window tkinter with mixin and function geometry

--- ui.py
+++ ui.py
@@ -1,27 +1,47 @@
 from tkinter import *
 
-#heredamos de tk en vez de utilizar root 
-  #entonces mainwindow se convertira en la parte que gestionara
-    #la parte principal de la interfaz
-class MainWindow(Tk):
-  #definimos el constructor
-  def __init__(self):
-    #utilizamos la funcion super para llamar de la superclase que estamos
-      #heredando ue es tk el metodo init que tiene internamente 
-      super().__init__()
-      #haciendo referencia a la intancia de la ventana podremos 
-        #ya mismo realizar modificaciones sobre ella
-      self.title("Gestor de clientes")
-      
-  #separamos la construccion de la venta en otro metodo 
+class CenterWidgetMixin:
   
+  #crearemos un metodo para que deje en el centro la ventana visual de tkinter
+  def center(self):
+    #actauliza la informacion con todos los elementos que hay dentro de la ventana
+    self.update()
+    #utilizamos la funcion geometry que es donde modificaremos
+      #parametros para posicionar la ventana del tkinter
+      
+    #ancho ventana
+    w = self.winfo_width()
+    #alto ventana
+    h = self.winfo_height()
+    
+    #ancho pantalla
+    ws = self.winfo_screenwidth()
+    
+    hs = self.winfo_screenheight()
+    
+    #calculamos el posicionamiento de la ventana con respecto al tamano de la pantalla
+    
+    x = int(ws/2 - w/2)
+    y = int(hs/2 - h/2)
+    
+    self.geometry(f"{w}x{h}+{x}+{y}")
+  
+class MainWindow(Tk, CenterWidgetMixin):
+  def __init__(self):
+
+      super().__init__()
+      self.title("Gestor de clientes")  
+      self.build()
+      self.center()
   def build(self):
     button = Button(self, text="Hola", command=self.hola)
     button.pack()
   
   def hola(self):
     print("Hola mundo!")
+  
+  
     
-if __name__ == "__main":
+if __name__ == "__main__":
   app = MainWindow()
   app.mainloop()
