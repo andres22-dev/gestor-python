2b615b0fa0ca2aaffa05c1d24f89af407826c885
andres22 <afpinchao8@misena.edu.co>
2024-05-14 23:43:34 -0500

    update run mode for visual  interface and console

--- run.py
+++ run.py
@@ -1,3 +1,6 @@
+import ui
+import sys
+
 #en este script sera el que ejecutara toda la aplicacion
 
 #importamos el menu
@@ -6,4 +9,9 @@ import menu
 #comprobamos que se esta ejecutando desde el principal
 if __name__ == "__main__":
   #del modulo menu ejecutamos la funcion iniciar 
-  menu.iniciar()
+  
+  if len(sys.argv) > 1 and sys.argv[1] == "-t":
+    menu.iniciar()
+  else:
+    app = ui.MainWindow()
+    app.mainloop()
